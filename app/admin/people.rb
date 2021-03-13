b7dc1e9e78314edc4202ef1dc178e5cb832c9c87
Adam Iskounen <adam.iskounen@artsymail.com>
2021-03-13 09:53:37 -0500

    chore: install activeadmin

---
+++ app/admin/people.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Person do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :birth, :death
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :birth, :death]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
 const { environment } = require('@rails/webpacker')
+const jquery = require('./plugins/jquery')
 const typescript =  require('./loaders/typescript')
 
 environment.loaders.prepend('typescript', typescript)
+environment.plugins.prepend('jquery', jquery)
 module.exports = environment
